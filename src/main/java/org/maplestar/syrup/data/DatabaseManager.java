a117bb1f8bdcd83a8121fc6c6faa8944854a9359
das_ <git@heap.mozmail.com>
2024-11-26 20:12:44 +0100

    Add command permissions
    Co-authored-by: maple <115593813+mycatsarefat369@users.noreply.github.com>

--- src/main/java/org/maplestar/syrup/data/DatabaseManager.java
+++ src/main/java/org/maplestar/syrup/data/DatabaseManager.java
@@ -56,7 +56,6 @@ public class DatabaseManager {
             statement.execute("CREATE TABLE IF NOT EXISTS BlockedChannels (guild_id BIGINT, channel_id BIGINT, PRIMARY KEY (guild_id, channel_id))");
             statement.execute("CREATE TABLE IF NOT EXISTS GuildSettings (guild_id BIGINT PRIMARY KEY, remove_old_roles BOOLEAN, add_on_join BOOLEAN)");
             statement.execute("CREATE TABLE IF NOT EXISTS LevelRoles (guild_id BIGINT, role_id BIGINT, level INTEGER, PRIMARY KEY (guild_id, role_id))");
-            statement.execute("CREATE TABLE IF NOT EXISTS ModeratorRoles (guild_id BIGINT, role_id BIGINT, PRIMARY KEY (guild_id, role_id))");
         } catch (SQLException exception) {
             logger.error("Could not create tables", exception);
             System.exit(1);
